fd437fdb39b98992a645e2dc6bdea58a15647dcd
Oscar Blumberg <nael@cerise.(none)>
2009-01-17 23:42:25 +0100

    Begining of an interpreter

--- src/org/jnizon/JNizon.java
+++ src/org/jnizon/JNizon.java
@@ -1,5 +1,8 @@
 package org.jnizon;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.antlr.runtime.ANTLRStringStream;
 import org.antlr.runtime.CommonTokenStream;
 import org.antlr.runtime.RecognitionException;
@@ -10,12 +13,10 @@ import org.antlr.runtime.tree.CommonTreeAdaptor;
 
 public class JNizon {
 
-	/**
-	 * @param args
-	 * @throws RecognitionException
-	 */
+	private static Context ctx;
+	
 	public static void main(String[] args) throws RecognitionException {
-		ANTLRStringStream str = new ANTLRStringStream("tz = Test[1,2,3];Show[tz];");
+		ANTLRStringStream str = new ANTLRStringStream("t = 3;q = 4;v = 8;q;");
 		SyntaxLexer lexer = new SyntaxLexer(str);
 		TokenStream tkstr = new CommonTokenStream(lexer);
 		SyntaxParser parser = new SyntaxParser(tkstr);
@@ -31,6 +32,47 @@ public class JNizon {
 		System.out.println("Finished.");
 		SyntaxTree tree = (SyntaxTree) result.getTree();
 		printTree(tree, 0);
+		Heap heap = new Heap();
+		ctx = heap.getContext(0);
+		CodeBlock main = (CodeBlock)convertTree(tree);
+		Expression expr = main.evaluate(ctx);
+		System.out.println("Result : " + expr);
+		//ctx.set("Times", function);
+	}
+	
+	public static Expression convertTree(CommonTree tree) {
+		if(tree.getType() == SyntaxParser.ROOT) {
+			CodeBlock block = new CodeBlock();
+			if (tree.getChildren() != null) {
+				for (Object child : tree.getChildren()) {
+					block.getStatements().add(convertTree((CommonTree)child));
+				}
+			}
+			return block;
+		} else if(tree.getType() == SyntaxParser.ASSIGNEMENT) {
+			Identifier lval = id((CommonTree)tree.getChild(0));
+			Expression rVal = convertTree((CommonTree)tree.getChild(1));
+			return new Assignment((Identifier)lval, rVal);
+		} else if(tree.getType() == SyntaxParser.INT) {
+			return new IntConstant(Integer.parseInt(tree.getText()));
+		}else if(tree.getType() == SyntaxParser.ID) {
+			return id(tree);
+		} else if(tree.getType() == SyntaxParser.FUNCTIONCALL) {
+			Identifier funcid = id((CommonTree)tree.getChild(0));
+			List<Expression> arguments = new ArrayList<Expression>();
+			for(int i = 1; i < tree.getChildCount(); i++) {
+				arguments.add(convertTree((CommonTree)tree.getChild(i)));
+			}
+			Expression func = ctx.get(funcid);
+			if(func instanceof Function)
+				return new FunctionCall((Function)func, arguments);
+			else throw new RuntimeException("Id : " + funcid + " is not a function.");
+		}
+		return null;
+	}
+	
+	public static Identifier id(CommonTree tree) {
+		return new Identifier(tree.getText());
 	}
 
 	public static void printTree(CommonTree tree, int indent) {
@@ -40,7 +82,7 @@ public class JNizon {
 			System.out.println("Null");
 			return;
 		}
-		System.out.println(tree.getText());
+		System.out.println(tree.getText() + " : " + tree.getToken().getType());
 		if (tree.getChildren() != null) {
 			for (Object child : tree.getChildren()) {
 				printTree((CommonTree) child, indent + 1);
