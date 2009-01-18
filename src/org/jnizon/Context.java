5f843556f3f3e21b82c0878f88fabe4d64462c41
Oscar Blumberg <nael@cerise.(none)>
2009-01-18 02:42:26 +0100

    Function call kinda working (not tested yet with args)

--- src/org/jnizon/Context.java
+++ src/org/jnizon/Context.java
@@ -4,41 +4,63 @@ import java.util.HashMap;
 
 public class Context extends HashMap<Integer, HeapItem> {
 	private static final long serialVersionUID = 4161192730488414955L;
-	
+
 	private int context_id = 0;
-	
-	public Context(int id) {
+	private Heap heap;
+	private Context parent;
+
+	public Context(int id, Heap heap) {
 		this.context_id = id;
+		this.heap = heap;
 	}
-	
+
+	public void setParent(Context parent) {
+		this.parent = parent;
+	}
+
+	public Context getParent() {
+		return parent;
+	}
+
 	public Expression get(Identifier id) {
 		HeapItem item = get(id.getName());
-		if(item == null) return id;
+		if (item == null) {
+			if(parent == null) return id;
+			return parent.get(id);
+		}
+			
 		return item.getValue();
 	}
-	
+
 	public HeapItem get(String key) {
 		return get(key.hashCode());
 	}
-	
+
 	public void put(int key, HeapItem value) {
 		put(Integer.valueOf(key), value);
 	}
-	
+
 	public void put(String key, HeapItem value) {
 		put(key.hashCode(), value);
 	}
-	
+
 	public void put(HeapItem value) {
 		put(value.getLabel(), value); // TODO: Change hashCode to MD4
 	}
-	
+
 	public void put(Identifier id, Expression expr) {
 		HeapItem item = new HeapItem(id.getName(), expr);
 		put(item);
 	}
-	
+
 	public int getContextId() {
 		return context_id;
+	}
+
+	public Context derivate() {
+		int childId = heap.newContext();
+		Context child = heap.getContext(childId);
+		child.setParent(this);
+		return child;
 	}
 }
--- src/org/jnizon/Interpreter.java
+++ src/org/jnizon/Interpreter.java
@@ -10,13 +10,14 @@ import org.antlr.runtime.Token;
 import org.antlr.runtime.TokenStream;
 import org.antlr.runtime.tree.CommonTree;
 import org.antlr.runtime.tree.CommonTreeAdaptor;
+import org.antlr.runtime.tree.ParseTree;
 
 public class Interpreter {
 	private CommonTreeAdaptor adaptor;
 	private static Heap heap;
 	private static Context global_context;
-	
-	public Interpreter() {
+
+	public Interpreter() throws RecognitionException {
 		adaptor = new CommonTreeAdaptor() {
 			@Override
 			public Object create(Token token) {
@@ -26,58 +27,69 @@ public class Interpreter {
 		
 		heap = new Heap();
 		global_context = heap.getContext(0);
+		
+		Identifier fid = new Identifier("Test");
+		CodeBlock fbdy = parse("4242;");
+		Function func = new Function(fid, fbdy, new Identifier[]{});
+		global_context.put(fid, func);
 	}
-	
+
 	public Expression evaluate(String code) throws RecognitionException {
+		CodeBlock main = parse(code);
+		return main.evaluate(global_context);
+	}
+
+	public CodeBlock parse(String code) throws RecognitionException {
 		ANTLRStringStream str = new ANTLRStringStream(code);
 		SyntaxLexer lexer = new SyntaxLexer(str);
 		TokenStream tkstr = new CommonTokenStream(lexer);
 		SyntaxParser parser = new SyntaxParser(tkstr);
 
 		parser.setTreeAdaptor(adaptor);
-		
+
 		System.out.println("Starting parsing...");
 		SyntaxParser.start_return result = parser.start();
 		System.out.println("Finished.");
-		
+
 		SyntaxTree tree = (SyntaxTree) result.getTree();
 		printTree(tree, 0);
-		
-		CodeBlock main = (CodeBlock)convertTree(tree);
-		return main.evaluate(global_context);
+
+		return (CodeBlock) convertTree(tree);
 	}
-	
+
 	public static Expression convertTree(CommonTree tree) {
-		if(tree.getType() == SyntaxParser.ROOT) {
+		if (tree.getType() == SyntaxParser.ROOT) {
 			CodeBlock block = new CodeBlock();
 			if (tree.getChildren() != null) {
 				for (Object child : tree.getChildren()) {
-					block.getStatements().add(convertTree((CommonTree)child));
+					block.getStatements().add(convertTree((CommonTree) child));
 				}
 			}
 			return block;
-		} else if(tree.getType() == SyntaxParser.ASSIGNEMENT) {
-			Identifier lval = id((CommonTree)tree.getChild(0));
-			Expression rVal = convertTree((CommonTree)tree.getChild(1));
-			return new Assignment((Identifier)lval, rVal);
-		} else if(tree.getType() == SyntaxParser.INT) {
+		} else if (tree.getType() == SyntaxParser.ASSIGNEMENT) {
+			Identifier lval = id((CommonTree) tree.getChild(0));
+			Expression rVal = convertTree((CommonTree) tree.getChild(1));
+			return new Assignment((Identifier) lval, rVal);
+		} else if (tree.getType() == SyntaxParser.INT) {
 			return new IntConstant(Integer.parseInt(tree.getText()));
-		}else if(tree.getType() == SyntaxParser.ID) {
+		} else if (tree.getType() == SyntaxParser.ID) {
 			return id(tree);
-		} else if(tree.getType() == SyntaxParser.FUNCTIONCALL) {
-			Identifier funcid = id((CommonTree)tree.getChild(0));
+		} else if (tree.getType() == SyntaxParser.FUNCTIONCALL) {
+			Identifier funcid = id((CommonTree) tree.getChild(0));
 			List<Expression> arguments = new ArrayList<Expression>();
-			for(int i = 1; i < tree.getChildCount(); i++) {
-				arguments.add(convertTree((CommonTree)tree.getChild(i)));
+			for (int i = 1; i < tree.getChildCount(); i++) {
+				arguments.add(convertTree((CommonTree) tree.getChild(i)));
 			}
 			Expression func = global_context.get(funcid);
-			if(func instanceof Function)
-				return new FunctionCall((Function)func, arguments);
-			else throw new RuntimeException("Id : " + funcid + " is not a function.");
+			if (func instanceof Function)
+				return new FunctionCall((Function) func, arguments);
+			else
+				throw new RuntimeException("Id : " + funcid
+						+ " is not a function.");
 		}
 		return null;
 	}
-	
+
 	public static Identifier id(CommonTree tree) {
 		return new Identifier(tree.getText());
 	}
