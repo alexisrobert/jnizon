be96ca718880be4ead03e34547119180e1880461
Alexis ROBERT <alexis.robert@gmail.com>
2009-01-18 20:44:38 +0100

    Add marking fields for a future Mark & Sweep GC implementation

--- src/org/jnizon/HeapItem.java
+++ src/org/jnizon/HeapItem.java
@@ -3,6 +3,7 @@ package org.jnizon;
 public class HeapItem {
 	private String label;
 	private Expression value;
+	private boolean marked = false;
 	
 	public HeapItem(String label) { this(label, null); }
 	public HeapItem(String label, Expression value) {
@@ -24,5 +25,13 @@ public class HeapItem {
 	
 	public Expression getValue() {
 		return value;
+	}
+	
+	public void setMarked(boolean mark) {
+		marked = mark;
+	}
+	
+	public boolean isMarked() {
+		return marked;
 	}
 }
