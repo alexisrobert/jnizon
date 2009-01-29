a5855c53cd220fb25f33fd44d86c985c23b4e434
Oscar Blumberg <nael@cerise.(none)>
2009-01-29 16:02:54 +0100

    Minor change to avoid double printing

--- src/org/jnizon/Shell.java
+++ src/org/jnizon/Shell.java
@@ -33,8 +33,8 @@ public class Shell {
 				System.out.print(String.format("Out[%d]= ", iteration));
 
 				try {
-					System.out.println(interpreter.evaluate(line));
-					//interpreter.evaluate(line);
+					//System.out.println(interpreter.evaluate(line));
+					interpreter.evaluate(line);
 					//Expression tree = interpreter.parse(line);
 					
 				} catch (RecognitionException e) {
